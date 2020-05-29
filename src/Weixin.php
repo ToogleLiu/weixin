19e08975f9604d0706282389cc80e4d3ec57325a
liushengtao <lst8696@gmail.com>
2020-05-29 09:47:47 +0800

    add func

--- src/Weixin.php
+++ src/Weixin.php
@@ -6,6 +6,16 @@ class Weixin
 {
     public function version()
     {
-        return '7.1.8';
+        return config('weixin.env') == 'dev' ? 'dev-master' : '7.1.8';
+    }
+
+    public function pushArr(array $from, array $to)
+    {
+        $from = array_column($from, null, 'id');
+        foreach ($to as &$value) {
+            $value['from'] = $from[$value['from_id']] ?: [];
+        }
+        unset($value);
+        return $to;
     }
 }
